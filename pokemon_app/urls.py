11e1dc84f956cd900c3ffa886cb62d456497f555
kaeriv93 <vsan93@yahoo.com>
2022-05-31 15:48:10 -0500

    Full crud achieved

--- pokemon_app/urls.py
+++ pokemon_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns =[
     path('', views.Home.as_view(), name='home'),
     path('pokemon/', views.PokemonList.as_view(), name='pokemon'),
     path('pokemon/new/', views.PokemonCreate.as_view(), name='pokemon_create'),
-    path('/pokemon/<int:pk>/', views.PokemonDetail.as_view(), name='pokemon_detail')
+    path('pokemon/<int:pk>/', views.PokemonDetail.as_view(), name='pokemon_detail'),
+    path('pokemon/<int:pk>/update', views.PokemonUpdate.as_view(), name='pokemon_update'),
+    path('pokemon/<int:pk>/delete', views.PokemonDelete.as_view(), name='pokemon_delete'),
 ]
--- pokemon_app/views.py
+++ pokemon_app/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from .models import Pokemon
+from django.urls import reverse
 
 class Home(TemplateView):
     template_name = 'home.html'
@@ -27,3 +28,16 @@ class PokemonCreate(CreateView):
 class PokemonDetail(DetailView):
     model = Pokemon
     template_name = 'pokemon_detail.html' 
+    
+class PokemonUpdate(UpdateView):
+    model = Pokemon
+    fields = ['name', 'sprite', 'index', 'bio', 'can_evolve']
+    template_name = 'pokemon_update.html'
+    
+    def get_success_url(self):
+        return reverse('pokemon_detail', kwargs={'pk': self.object.pk})
+
+class PokemonDelete(DeleteView):
+    model = Pokemon
+    template_name = 'pokemon_delete_confirmation.html'
+    success_url = '/pokemon/'
