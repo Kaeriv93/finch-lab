6a2ce62d19a404ab911cba77255c2489d6de105b
kaeriv93 <vsan93@yahoo.com>
2022-06-01 20:12:07 -0500

    added django settings to deploy

--- pokemon/settings.py
+++ pokemon/settings.py
@@ -112,3 +112,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'pokemon_app/static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
